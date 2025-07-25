25ec85067402e25e780c4256871cc74c3c6b9701
Anupam Chetia <stealthanupam@gmail.com>
2025-07-25 11:50:11 -0400

    Modularity testing6

--- app/services/github_service.py
+++ app/services/github_service.py
@@ -10,7 +10,7 @@ tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 
 async def get_pull_request_details(repo, pr_number, token):
     url = f"https://api.github.com/repos/{repo}/pulls/{pr_number}"
-    headers = {"Authorization": f"token {token}"}
+    headers = {"Authorization": f"Bearer {token}"}
     async with httpx.AsyncClient() as client:
         response = await client.get(url, headers=headers)
         pr_details = response.json()
@@ -29,13 +29,12 @@ async def get_pull_request_details(repo, pr_number, token):
 
 async def post_comment(repo, pr_number, comment, token):
     url = f"https://api.github.com/repos/{repo}/issues/{pr_number}/comments"
-    headers = {"Authorization": f"token {token}"}
+    headers = {"Authorization": f"Bearer {token}"}
     data = {"body": comment}
     async with httpx.AsyncClient() as client:
         response = await client.post(url, headers=headers, json=data)
     with open('post_comment.json', 'w') as file:
         json.dump(response.json(), file, indent=4)
-    print("I am at B")
     return response.json()
 
 
